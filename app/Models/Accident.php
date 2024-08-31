cb8e7c925561e01896e5f79e67687a85823d0789
ikbal-mahmud-sohan <devsohan007@gmail.com>
2024-08-31 16:16:22 +0600

    update

--- app/Models/Accident.php
+++ app/Models/Accident.php
@@ -10,11 +10,7 @@ class Accident extends Model
     use HasFactory;
     protected $fillable = [
     'month',
-    'date',
-    'name',
-    'designation',
-    'supervisor',
-    'department',   
+    'date', 
     'type_of_accident',
     'description',
     'zone_location',
@@ -30,9 +26,25 @@ class Accident extends Model
     'responsible_name',
     'deadline',
     'status',
-    'verified_image'
+    'verified_image',
+    // 'name',
+    // 'designation',
+    // 'supervisor',
+    // 'department',  
+    'type_of_employee',
+    'site_name',
+    'time_date',
+    'incident_category',
+    'immidiate_cause',
+    'incident_location',
+    'incident_descriptions',
+    'investigation_lead',
+    'attachment'
     ];
     protected $casts = [
         'verified_image' => 'array', 
+        'attachment' => 'array',
+        'property_damaged' => 'boolean',
+        'status' => 'boolean', 
     ];
 }
