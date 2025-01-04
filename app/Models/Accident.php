b2961e563a3469712c2311c0d01a11a372e8a5bd
ikbal-mahmud-sohan <devsohan007@gmail.com>
2025-01-05 00:33:41 +0600

    update filed

--- app/Models/Accident.php
+++ app/Models/Accident.php
@@ -13,11 +13,9 @@ class Accident extends Model
 
     use HasFactory;
     protected $fillable = [
-    'month',
     'date', 
     'type_of_accident',
-    'description',
-    'zone_location',
+    'incident_location',
     'precise_location',
     'injury_type',
     'affected_body_parts',
@@ -28,14 +26,10 @@ class Accident extends Model
     'remarks',
     'type_of_victim_employee',
     'responsible_name',
-    'deadline',
     'status',
     'verified_image',
     'site_name',
-    'time_date',
-    'incident_category',
     'immidiate_cause',
-    'incident_location',
     'incident_descriptions',
     'investigation_lead',
     'attachment',
