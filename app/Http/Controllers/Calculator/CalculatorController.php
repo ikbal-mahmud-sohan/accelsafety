42918d2dd305f22695f902c2fc28bb53eeb855f9
Md Aktarozzaman <aktarozzaman8@gmail.com>
2025-02-27 23:13:52 +0600

    calculator api done

--- app/Http/Controllers/Calculator/CalculatorController.php
+++ app/Http/Controllers/Calculator/CalculatorController.php
@@ -117,5 +117,101 @@ class CalculatorController extends Controller
         return $validated;
     }
 
+    //severity_rate_calculator
+    public function severity_rate_calculator(Request $request){
+
+        $validated= $request->validate([
+            'number_of_man_days_lost'=>'required',
+            'total_man_hours_worked'=>'required',
+        ]);
+
+        return $validated;
+    }
+
+    //incidence_rate_calculator
+    public function incidence_rate_calculator(Request $request){
+
+        $validated= $request->validate([
+            'number_of_lost_time_injuries'=>'required',
+            'average_number_of_persons_employed'=>'required',
+        ]);
+
+        return $validated;
+    }
+    //ambient_noise_level_calculator
+    public function ambient_noise_level_calculator(Request $request){
+
+        $validated= $request->validate([
+            'category_of_area'=>'required',
+            'time_of_day'=>'required',
+        ]);
+
+        return $validated;
+    }
+
+    //generalized_anxiety_disorder_assessment
+    public function generalized_anxiety_disorder_assessment(Request $request){
+
+        $validated= $request->validate([
+            'feeling_nervous_anxious_or_on_edge'=>'required',
+            'not_being_able_to_stop_or_control_worrying'=>'required',
+            'worrying_too_much_about_different_things'=>'required',
+            'trouble_relaxing'=>'required',
+            'being_so_restless_that_hard_to_sit_still'=>'required',
+            'becoming_easily_annoyed_or_irritable'=>'required',
+            'feeling_afraid_as_if_something_awful_might_happen'=>'required',
+        ]);
+
+        return $validated;
+    }
+
+    //daily_drinking_water_intake_calculator
+    public function daily_drinking_water_intake_calculator(Request $request){
+
+        $validated= $request->validate([
+            'life_stage_sex'=>'required',
+            'age'=>'required'
+
+        ]);
+
+        return $validated;
+    }
+
+    //osha_total_recordable_incident_rate_trir_calculator
+    public function osha_total_recordable_incident_rate_trir_calculator(Request $request){
+
+        $validated= $request->validate([
+            'number_of_recordable_injuries_and_illnesses'=>'required',
+            'total_employee_hours_worked'=>'required'
+
+        ]);
+
+        return $validated;
+    }
+
+    //osha_dart_rate_calculator
+    public function osha_dart_rate_calculator(Request $request){
+
+        $validated= $request->validate([
+            'total_number_of_DART_incidents'=>'required',
+            'total_hours_worked'=>'required'
+
+        ]);
+
+        return $validated;
+    }
+
+    //nema_ratings_and_ip_equivalency_calculator
+    public function nema_ratings_and_ip_equivalency_calculator(Request $request){
+
+        $validated= $request->validate([
+            'input_type'=>'required',
+            'rating'=>'required'
+
+        ]);
+
+        return $validated;
+    }
+
 
 }
